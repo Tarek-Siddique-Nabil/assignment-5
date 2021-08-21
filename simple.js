171038bc740334d4ecc254ecf9b2bee650a54a8c
Tarek-Siddique-Nabil <nabilh420@gmail.com>
2021-08-21 16:39:30 +0600

    update

---
+++ simple.js
@@ -0,0 +1,102 @@
+// function updatePrice(product, price){
+//     const productPriceOutput = document.getElementById(product +'-cost');
+//     let productPrice = productPriceOutput.innerText;
+//     if(productPrice == 0){
+//         productPrice = parseInt(productPrice + price) ;
+
+//     }
+//     else if(productPrice != 0){
+//         productPrice = parseInt(productPrice + price) ;
+//     }
+
+//     productPriceOutput.innerText = productPrice;
+
+
+// }
+
+function updatePrice(product, price) {
+    const itemPrice = document.getElementById(product + '-cost')
+    itemPrice.innerText = price;
+
+   
+
+
+
+
+calculateTotal();
+  
+}
+
+document.getElementById('memory-btn1').addEventListener('click', function () {
+    updatePrice('memory', 0);
+})
+document.getElementById('memory-btn2').addEventListener('click', function () {
+    updatePrice('memory', 180);
+})
+document.getElementById('storage-btn1').addEventListener('click', function () {
+    updatePrice('storage', 0);
+})
+document.getElementById('storage-btn2').addEventListener('click', function () {
+    updatePrice('storage', 100);
+})
+document.getElementById('storage-btn3').addEventListener('click', function () {
+    updatePrice('storage', 180);
+})
+document.getElementById('delivery-btn1').addEventListener('click', function () {
+    updatePrice('delivery', 0);
+})
+document.getElementById('delivery-btn2').addEventListener('click', function () {
+    updatePrice('delivery', 20);
+})
+
+function getInput(product){
+    const inputValue = document.getElementById(product +'-cost').innerText;
+     const value = parseInt(inputValue);
+     return value;
+   
+}
+
+function calculateTotal() {
+    const basePrice = getInput('base');
+    const memoryPrice = getInput('memory');
+    const storagePrice = getInput('storage');
+    const deliveryPrice = getInput('delivery');
+  
+   const total =basePrice + memoryPrice + storagePrice + deliveryPrice ;
+   document.getElementById('total-cost').innerText = total;
+   document.getElementById('total').innerText = total;
+   
+}
+
+document.getElementById('promo-code').addEventListener('click',function() {
+   const input = document.getElementById('promo-input').value;
+   const code = 'stevekaku';
+   const total = document.getElementById('total-cost').innerText;
+   const success = document.getElementById('promo-success');
+   const fail = document.getElementById('promo-error');
+    document.getElementById('promo-input').value = '' ;
+  if(input == code){
+       const last = parseFloat(total-((total/100)*20));
+       document.getElementById('total').innerText= last;
+       success.style.display ='block';
+        fail.style.display = 'none';
+       
+   }
+   else{
+    fail.style.display = 'block';
+    success.style.display = 'none';
+   }
+   
+})
+    
+ 
+
+    
+     
+
+
+
+
+
+
+
